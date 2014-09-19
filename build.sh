32e7a61ea00efd7c3c195e97643f127e9d31ac30
Charles Cooper <charles@lenovo-ideapad>
2014-09-19 08:03:55 -0700

    compile itch.h

---
+++ build.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++1y -ferror-limit=2 \
+	main.cpp bufferedreader.cpp
--- itch.h
+++ itch.h
@@ -1,5 +1,5 @@
-
-enum class itch_msg_t 
+#pragma once
+enum class itch_t
 {
 	SYSEVENT = 'S',
 	STOCK_DIRECTORY = 'R',
@@ -22,38 +22,61 @@ enum class itch_msg_t
 	NET_ORDER_IMBALANCE = 'I',
 	RETAIL_PRICE_IMPROVEMENT = 'N'
 };
-template<itch_t __code>
-struct itch_message
-{};
-
 using MSG = itch_t;
 template<MSG __type> unsigned char netlen;
+template<> constexpr unsigned char
 netlen<MSG::SYSEVENT>					= 12;
+template<> constexpr unsigned char
 netlen<MSG::STOCK_DIRECTORY>			= 39;
+template<> constexpr unsigned char
 netlen<MSG::TRADING_ACTION>				= 25;
+template<> constexpr unsigned char
 netlen<MSG::REG_SHO_RESTRICT>			= 20;
+template<> constexpr unsigned char
 netlen<MSG::MPID_POSITION>				= 26;
+template<> constexpr unsigned char
 netlen<MSG::MWCB_DECLINE>				= 35;
+template<> constexpr unsigned char
 netlen<MSG::MWCB_STATUS>				= 12;
-netlen<MSG::IPO_UPDATE>					= 28;
+template<> constexpr unsigned char
+netlen<MSG::IPO_QUOTE_UPDATE>			= 28;
+template<> constexpr unsigned char
 netlen<MSG::ADD_ORDER>					= 36;
+template<> constexpr unsigned char
 netlen<MSG::ADD_ORDER_MPID>				= 40;
+template<> constexpr unsigned char
 netlen<MSG::EXECUTE_ORDER>				= 31;
+template<> constexpr unsigned char
 netlen<MSG::EXECUTE_ORDER_WITH_PRICE>	= 36;
-netlen<MSG::REDUCE_ORDER>-				= 23;
-netlen<MSG::DELETE_ORDER>-				= 19;
-netlen<MSG::REPLACE_ORDER>-				= 35;
-netlen<MSG::TRADE>-						= 44;
+template<> constexpr unsigned char
+netlen<MSG::REDUCE_ORDER>				= 23;
+template<> constexpr unsigned char
+netlen<MSG::DELETE_ORDER>				= 19;
+template<> constexpr unsigned char
+netlen<MSG::REPLACE_ORDER>				= 35;
+template<> constexpr unsigned char
+netlen<MSG::TRADE>						= 44;
+template<> constexpr unsigned char
 netlen<MSG::CROSS_TRADE>				= 40;
+template<> constexpr unsigned char
 netlen<MSG::BROKEN_TRADE>				= 19;
+template<> constexpr unsigned char
 netlen<MSG::NET_ORDER_IMBALANCE>		= 50;
+template<> constexpr unsigned char
 netlen<MSG::RETAIL_PRICE_IMPROVEMENT>	= 20;
+
+template<itch_t __code>
+struct itch_message
+{
+	static constexpr itch_t code = __code;
+};
 
 enum class BUY_SELL : char {
 	BUY='B',
 	SELL='S'
 };
-typedef struct itch_message<MSG::ADD_ORDER>
+template<>
+struct itch_message<MSG::ADD_ORDER>
 {
 	uint64_t timestamp;
 	uint64_t oid;
@@ -61,52 +84,54 @@ typedef struct itch_message<MSG::ADD_ORDER>
 	uint32_t qty;
 	uint16_t stock_locate;
 	BUY_SELL buy;
-} add_order_t;
-typedef struct itch_message<MSG::ADD_ORDER_MPID>
+};
+using add_order_t = itch_message<MSG::ADD_ORDER>;
+template<>
+struct itch_message<MSG::ADD_ORDER_MPID>
 {
 	add_order_t add_msg;
 	char mpid[4];
-} add_order_mpid_t;
-typedef struct itch_message<MSG::EXECUTE_ORDER>
+};
+using add_order_mpid_t = itch_message<MSG::ADD_ORDER_MPID>;
+template<>
+struct itch_message<MSG::EXECUTE_ORDER>
 {
 	uint64_t oid;
 	uint64_t timestamp;
 	uint32_t exec_shares;
 	uint16_t stock_locate;
-} execute_order_t;
-typedef struct itch_message<MSG::EXECUTE_ORDER_WITH_PRICE>
+};
+using execute_order_t = itch_message<MSG::EXECUTE_ORDER>;
+template<>
+struct itch_message<MSG::EXECUTE_ORDER_WITH_PRICE>
 {
 	execute_order_t exec;
-	// printable
-	// price
-} execute_with_price_t;
-typedef struct itch_message<MSG::REDUCE_ORDER>
+	// ignore printable and price
+};
+using execute_with_price_t = itch_message<MSG::EXECUTE_ORDER_WITH_PRICE>;
+template<>
+struct itch_message<MSG::REDUCE_ORDER>
 {
 	uint64_t oid;
 	uint64_t timestamp;
 	uint32_t shares;
-} order_reduce_t;
-typedef struct itch_message<MSG::DELETE_ORDER>
+};
+using order_reduce_t = itch_message<MSG::REDUCE_ORDER>;
+template<>
+struct itch_message<MSG::DELETE_ORDER>
 {
 	uint64_t oid;
 	uint64_t timestamp;
-} order_delete_t;
-typedef struct itch_message<MSG::REPLACE_ORDER>
+};
+using order_delete_t = itch_message<MSG::DELETE_ORDER>;
+template<>
+struct itch_message<MSG::REPLACE_ORDER>
 {
 	uint64_t oid;
 	uint64_t new_order_id;
 	uint64_t new_price;
 	uint32_t new_shares;
-} order_replace_t;
-typedef struct itch_message<MSG::TRADE>
-{} trade_t;
-typedef struct itch_message<MSG::CROSS_TRADE>
-{} cross_t;
-typedef struct itch_message<MSG::BROKEN_TRADE>
-{} broken_trade_t;
-typedef struct itch_message<MSG::NET_ORDER_IMBALANCE>
-{} noii_t;
-typedef struct itch_message<MSG::RETAIL_PRICE_IMPROVEMENT>
-{} rpii_t;
+};
+using order_replace_t = itch_message<MSG::REPLACE_ORDER>;
 
 
